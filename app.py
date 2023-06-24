8db25689b20647e1b82a70c84c6d8769af0754b1
Benyamain <yacoobbenyamain@gmail.com>
2023-06-24 17:41:12 -0400

    Filtering results by page content

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask, request
 from search import search
 import html
+from filter import Filter
 
 app = Flask(__name__)
 
-search_template = """
+styles = """
+<style>
+.site {
+    font-size: .8rem;
+    color:green;
+}
+
+.snippet {
+    font-size: .9rem;
+    color: gray;
+    margin-bottom: 30px;
+}
+</style>
+"""
+
+search_template = styles + """
 <form action="/" method="post">
     <input type="text" name="query">
     <input type="submit" value="Search">
@@ -22,6 +38,8 @@ def show_search_form():
 
 def run_search(query):
     results = search(query)
+    filter = Filter(results)
+    results = filter.filter()
     rendered = search_template
     # Not render random html that exists within the snippet
     results['snippet'] = results['snippet'].apply(lambda x: html.escape(x))
