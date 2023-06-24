a41b02ece7450eba36f69136e8c72b6616773a7c
Benyamain <yacoobbenyamain@gmail.com>
2023-06-24 17:51:19 -0400

    Ranked the ad websites and bad domains as poor

--- filter.py
+++ filter.py
@@ -2,10 +2,25 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 from settings import *
 
+with open('blacklist.txt') as f:
+    bad_domain_list = set(f.read().split("\n"))
+
 def get_page_content(row):
     soup = BeautifulSoup(row['html'])
     text = soup.get_text()
     return text
+
+def tracker_urls(row):
+    soup = BeautifulSoup(row['html'])
+    scripts = soup.find_all('script', {"src": True})
+    srcs = [s.get('src') for s in scripts]
+
+    links = soup.find_all('a', {"href": True})
+    href = [l.get('href') for l in links]
+    all_domains = [urlparse(s).hostname for s in srcs + href]
+    bad_domains = [a for a in all_domains if a in bad_domains]
+
+    return len(bad_domains)
 
 class Filter():
     def __init__(self, results):
@@ -20,8 +35,14 @@ class Filter():
         word_count[word_count != RESULT_COUNT] = 0
         self.filtered['rank'] += word_count
 
+    def tracker_filter(self):
+        tracker_count = self.filtered.apply(tracker_urls, axis=1)
+        tracker_count[tracker_count > tracker_count.median()] = RESULT_COUNT * 2
+        self.filtered['rank'] += tracker_count
+
     def filter(self):
         self.content_filter()
+        self.tracker_filter()
         self.filtered = self.filtered.sort_values('rank', ascending=True)
         self.filtered['rank'] = self.filtered['rank'].round()
         return self.filtered
